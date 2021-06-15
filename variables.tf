50292dbf29bee4b547ff18c0aae2551e8445bb27
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-15 17:03:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
-
+variable "glue_database" {
+  type = string
+  default = ""
+}
+variable "s3_crawler" {
+  type = string
+  default = ""
+}
+variable "bucket_name" {
+  type = string
+  default = ""
+}
+variable "region_name" {
+  type = string
+  default = ""
+}
