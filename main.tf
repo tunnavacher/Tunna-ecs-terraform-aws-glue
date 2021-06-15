00258e6f9b156a2f75962b5fcd95defcda9b39d1
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-15 17:22:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_glue_catalog_database" "glue_catalog_database" {
 }
 
 resource "aws_glue_crawler" "crawler" {
-  database_name = glue_catalog_database.aws_glue_catalog_database.name
+  database_name = aws_glue_catalog_database.aws_glue_catalog_database.name
   name          = "var.s3_crawler"
   role          = "arn:aws:iam::517941526833:role/glue_to_s3"
   s3_target {
